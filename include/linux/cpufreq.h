e15b7095feebdb60d58ec2a26e46a022c28ccf3e
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2011-09-22 11:50:20 -0300

    Fixed some source code indentation

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -371,16 +371,16 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LAGFREE)
 extern struct cpufreq_governor cpufreq_gov_lagfree;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_lagfree)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lagfree)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMOOTHASS)
 extern struct cpufreq_governor cpufreq_gov_smoothass;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smoothass)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smoothass)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
 extern struct cpufreq_governor cpufreq_gov_smartass;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_smartass)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartass)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_interactive)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #endif
 
 
